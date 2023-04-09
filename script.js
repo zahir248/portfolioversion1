642ac84414c55f0ed37ab86d86c33591b34f3479
Muhammad Zahiruddin <90888537+zahir248@users.noreply.github.com>
2023-04-09 18:50:34 +0800

    first commit

---
+++ script.js
@@ -0,0 +1,9 @@
+// Open and close sidebar
+function openNav() {
+  document.getElementById("mySidebar").style.width = "100%";
+  document.getElementById("mySidebar").style.display = "block";
+}
+
+function closeNav() {
+  document.getElementById("mySidebar").style.display = "none";
+}

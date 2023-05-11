e8ece263d1ae0ba6c21fec9c257cb6c1d533be59
Muhammad Zahiruddin <90888537+zahir248@users.noreply.github.com>
2023-05-12 02:07:36 +0800

    update

--- app.js
+++ app.js
@@ -1,1 +1,36 @@
 AOS.init();
+
+const text = "Hello! வணக்கம்! 你好!";
+const words = text.split(" ");
+const colors = ["darkgreen", "darkblue", "darkred"];
+let index = 0;
+let isDeleting = false;
+let speed = 200;
+const typingText = document.querySelector(".typing-text");
+
+function type() {
+  const currentWord = words[index % words.length];
+  const currentColor = colors[index % colors.length];
+  if (isDeleting) {
+    typingText.textContent = currentWord.substring(0, typingText.textContent.length - 1);
+  } else {
+    typingText.textContent = currentWord.substring(0, typingText.textContent.length + 1);
+  }
+  typingText.style.color = currentColor;
+  typingText.style.fontSize = "5em"; // Set the font size to 2em
+  if (!isDeleting && typingText.textContent === currentWord) {
+    isDeleting = true;
+    speed = 500;
+  } else if (isDeleting && typingText.textContent === "") {
+    isDeleting = false;
+    index++;
+    speed = 200;
+  }
+
+  setTimeout(type, speed);
+}
+
+type();
+
+
+

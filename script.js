58341aff2cc41360fa7a2198de8e82656586b818
Muhammad Zahiruddin <90888537+zahir248@users.noreply.github.com>
2023-05-13 18:54:49 +0800

    update

--- script.js
+++
@@ -1,9 +0,0 @@
-// Open and close sidebar
-function openNav() {
-  document.getElementById("mySidebar").style.width = "100%";
-  document.getElementById("mySidebar").style.display = "block";
-}
-
-function closeNav() {
-  document.getElementById("mySidebar").style.display = "none";
-}
